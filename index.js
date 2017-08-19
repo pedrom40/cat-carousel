4e111c386cab945f6e177f9ed7a907cf5cfd37c8
pedrom40 <pedro.morin1980@gmail.com>
2017-08-19 08:12:11 -0500

    add keyboard interactivity

--- index.js
+++ index.js
@@ -11,9 +11,17 @@ function catCarousel () {
 
   $('.thumbnails').on('click', 'a', event => {
 
+    // save target and others
+    const clickedA = $(event.currentTarget);
+    const otherAs = $('.thumbnails a').not(clickedA);
+
     // save hero and selected images
     const heroImg = $('.hero img');
     const selectedImg = $(event.currentTarget).find('img');
+
+    // update aria values
+    $(clickedA).attr('aria-pressed', true);
+    $(otherAs).attr('aria-pressed', false);
 
     // update src and alt attributes
     $(heroImg).attr('src', $(selectedImg).attr('src'));
