74e3ab6910e0521f3c9c62c098a7bcd7165774eb
pedrom40 <pedro.morin1980@gmail.com>
2017-08-19 08:23:23 -0500

    add thinkful solution

--- index.js
+++ index.js
@@ -24,4 +24,27 @@ function catCarousel () {
 
 }
 
+/* Thinkful solution
+
+  **
+    Although simpler, I didn't go this route because it doesn't take
+    advantage of event delegation
+  **
+
+  function handleThumbnailClicks() {
+    $('.thumbnail').on('click', function(event) {
+      const imgSrc = $(event.currentTarget).find('img').attr('src');
+      const imgAlt = $(event.currentTarget).find('img').attr('alt');
+
+      $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+
+      // Accomplishes the same as line 6 with only one use off .attr()
+      // by passing it an object with multiple properties.
+      // See: http://api.jquery.com/attr/#attr-attributes
+      // $('.hero img').attr({'src': imgSrc, 'alt': imgAlt});
+    });
+  }
+
+*/
+
 $(catCarousel)
