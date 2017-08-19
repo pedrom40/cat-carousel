6f4cae7dec51e205d161b6444c4516d771cae060
pedrom40 <pedro.morin1980@gmail.com>
2017-08-19 07:51:36 -0500

    update hero image with selected img src and alt info

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 'use strict';
 
 function catCarousel () {
-  console.log('catCarousel');
+
+  /*
+    REQUIREMENTS
+    1. User selects thumbnail with mouse or keyboard, selected img should
+    replace hero img
+    2. Hero image alt text should be updated as well
+  */
+
+  $('.thumbnails').on('click', 'a', event => {
+
+    // save hero and selected images
+    const heroImg = $('.hero img');
+    const selectedImg = $(event.currentTarget).find('img');
+
+    // update src and alt attributes
+    $(heroImg).attr('src', $(selectedImg).attr('src'));
+    $(heroImg).attr('alt', $(selectedImg).attr('alt'));
+
+  });
+
 }
 
 $(catCarousel)
